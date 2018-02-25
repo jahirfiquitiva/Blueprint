d998684b6e02038ed53d9c485428d8031bc9f9de
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-02-24 21:34:49 -0500

    Attempt to fix issues with request on premarshmallow devices

--- library/src/main/kotlin/jahirfiquitiva/libs/blueprint/quest/utils/AppsLoader.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/blueprint/quest/utils/AppsLoader.kt
@@ -2,6 +2,8 @@ package jahirfiquitiva.libs.blueprint.quest.utils
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.content.pm.ResolveInfo
 import jahirfiquitiva.libs.blueprint.helpers.utils.BPLog
 import jahirfiquitiva.libs.blueprint.quest.App
 import java.util.ArrayList
@@ -12,40 +14,49 @@ internal fun Context.getInstalledApps(
         onProgress: (progress: Int) -> Unit = {}
                                      ): ArrayList<App> {
     
-    val packageList = packageManager.queryIntentActivities(
-            Intent("android.intent.action.MAIN")
-                    .addCategory("android.intent.category.LAUNCHER"), 0)
-    
-    val list = ArrayList(packageList.distinct())
-    list.sortWith(NameComparator(packageManager))
+    val packagesList = try {
+        packageManager.queryIntentActivities(
+                Intent("android.intent.action.MAIN")
+                        .addCategory("android.intent.category.LAUNCHER"),
+                PackageManager.GET_RESOLVED_FILTER)
+    } catch (e: Exception) {
+        ArrayList<ResolveInfo>()
+    }
     
     val apps = ArrayList<App>()
     
     var loaded = 0
     var filtered = 0
-    for (ri in packageList) {
-        val riPkg = ri.activityInfo.packageName
-        val launchStr = riPkg + "/" + ri.activityInfo.name
+    
+    if (packagesList.isNotEmpty()) {
         
-        if (filter.contains(launchStr) || packageName == riPkg) {
-            filtered++
-            continue
-        }
+        val list = ArrayList(packagesList.distinct())
+        list.sortWith(NameComparator(packageManager))
         
-        var name: CharSequence? = ri.loadLabel(packageManager)
-        if (name == null) name = ri.activityInfo.packageName
-        
-        val appToAdd = App(
-                this.getLocalizedName(launchStr, name.toString()), launchStr,
-                ri.activityInfo.packageName)
-        appToAdd.loadIcon(this)
-        apps.add(appToAdd)
-        loaded++
-        
-        try {
-            val percent = loaded * 100 / list.size
-            onProgress(percent)
-        } catch (e: Exception) {
+        for (ri in packagesList) {
+            val riPkg = ri.activityInfo.packageName
+            val launchStr = riPkg + "/" + ri.activityInfo.name
+            
+            if (filter.contains(launchStr) || packageName == riPkg) {
+                filtered++
+                continue
+            }
+            
+            var name: CharSequence? = ri.loadLabel(packageManager)
+            if (name == null) name = ri.activityInfo.packageName
+            
+            val appToAdd = App(
+                    this.getLocalizedName(launchStr, name.toString()), launchStr,
+                    ri.activityInfo.packageName)
+            appToAdd.loadIcon(this)
+            apps.add(appToAdd)
+            loaded++
+            
+            try {
+                val percent = loaded * 100 / list.size
+                onProgress(percent)
+            } catch (e: Exception) {
+            }
         }
     }
     
