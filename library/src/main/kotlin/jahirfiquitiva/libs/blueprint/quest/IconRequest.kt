ab2dafdeea1173da288f4e7da3f90b995edbac3d
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-06-04 10:09:27 -0500

    Fix duplicated logs showing for no duplicated components

--- library/src/main/kotlin/jahirfiquitiva/libs/blueprint/quest/IconRequest.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/blueprint/quest/IconRequest.kt
@@ -337,10 +337,8 @@ class IconRequest private constructor() {
         
         if (builder?.debug == true) {
             componentsCount.forEach {
-                val times = it.second
-                if (times > 1) {
-                    BL.w("Component \"${it.first}\" is duplicated $times time(s)")
-                }
+                if (it.second > 1)
+                    BL.w("Component \"${it.first}\" is duplicated ${it.second} time(s)")
             }
         }
         
