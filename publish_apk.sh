3bb62345c501abfa7295c86fd3546b5d797ced10
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-07-24 13:16:04 -0500

    Test telegram updates report

--- publish_apk.sh
+++ publish_apk.sh
@@ -16,7 +16,7 @@ if [ "$TRAVIS_PULL_REQUEST" = false ]; then
 		for apk in $(find *.apk -type f); do
 			apkName="${apk::-4}"
 			printf "Uploading: $apkName.apk ...\n"
-			upload="$(curl -s -X POST https://uploads.github.com/repos/${TRAVIS_REPO_SLUG}/releases/${releaseId}/assets?access_token=${GITHUB_API_KEY}&name=${apkName}.apk --header 'Content-Type: application/zip' --upload-file ${apkName}.apk)"
+			upload=`curl "https://uploads.github.com/repos/jahirfiquitiva/Blueprint/releases/12081257/assets?access_token=1dd2977505fc5a67106c96022f0e207bbb55bdbb&name=file.zip" --header 'Content-Type: application/zip' --upload-file file.zip  -X POST`
 			printf "\nResult: $upload\n"
 			urlText="$(echo "$upload" | jq --raw-output ".browser_download_url")"
 			if [ "$urlText" ]; then
