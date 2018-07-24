c9ef3d3bb45e2aaf251122858852d3b714117b3d
jahirfiquitiva <jahir.fiquitiva@gmail.com>
2018-07-24 12:30:31 -0500

    Test Telegram updates reports

--- tele.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-tagged="$(curl https://api.github.com/repos/${TRAVIS_REPO_SLUG}/releases/latest)"
-relname="$(echo "$tagged" | jq --raw-output ".tag_name")"
-repoName=$(echo $TRAVIS_REPO_SLUG | cut -f2 -d/)
-teleMess="Latest version for *$repoName* is ${relname//\"}"
-printf "Sending message: $teleMess"
-curl "https://api.telegram.org/bot${TEL_BOT_KEY}/sendMessage?chat_id=@JFsDashSupport&text=${teleMess}&parse_mode=Markdown"
