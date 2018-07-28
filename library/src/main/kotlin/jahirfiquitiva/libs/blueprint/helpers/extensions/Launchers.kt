724d17e55a6f7473832bd6e1ac17a5821f92d5ff
Till Kottmann <deletescape@gmail.com>
2018-07-28 18:55:05 +0200

    Add support for Lawnchair (#116)

--- library/src/main/kotlin/jahirfiquitiva/libs/blueprint/helpers/extensions/Launchers.kt
+++ library/src/main/kotlin/jahirfiquitiva/libs/blueprint/helpers/extensions/Launchers.kt
@@ -57,13 +57,6 @@ internal val Context.supportedLaunchers: ArrayList<Launcher>
             "Aviate Launcher", arrayOf("com.tul.aviate"),
             color(R.color.aviate_launcher_color)),
         Launcher(
-            "lineageos",
-            "LineageOS Theme Engine",
-            arrayOf(
-                "org.cyanogenmod.theme.chooser", "org.cyanogenmod.theme.chooser2",
-                "com.cyngn.theme.chooser"),
-            color(R.color.cm_theme_engine_color)),
-        Launcher(
             "go",
             "Go Launcher", arrayOf("com.gau.go.launcherex"),
             color(R.color.go_launcher_color)),
@@ -91,6 +84,16 @@ internal val Context.supportedLaunchers: ArrayList<Launcher>
             "l",
             "L Launcher", arrayOf("com.l.launcher"),
             color(R.color.l_launcher_color)),
+        Launcher(
+            "lawnchair",
+            "Lawnchair", arrayOf("ch.deletescape.lawnchair", "ch.deletescape.lawnchair.plah",
+                "ch.deletescape.lawnchair.ci", "ch.deletescape.lawnchair.dev"),
+            color(R.color.lawnchair_launcher_color)),
+        Launcher(
+                "lineageos", "LineageOS Theme Engine",
+            arrayOf("org.cyanogenmod.theme.chooser", "org.cyanogenmod.theme.chooser2",
+                    "com.cyngn.theme.chooser"),
+            color(R.color.cm_theme_engine_color)),
         Launcher(
             "lucid",
             "Lucid Launcher", arrayOf("com.powerpoint45.launcher"),
@@ -153,24 +156,25 @@ fun Context.executeLauncherIntent(launcherKey: String) {
                 3 -> executeApexLauncherIntent()
                 4 -> executeAtomLauncherIntent()
                 5 -> executeAviateLauncherIntent()
-                6 -> executeLineageOSThemeEngineIntent()
-                7 -> executeGoLauncherIntent()
-                8, 18 -> executeIconPacksNotSupportedIntent()
-                9 -> executeHoloLauncherIntent()
-                10 -> executeHoloLauncherICSIntent()
-                11 -> executeKkLauncherIntent()
-                12 -> executeLgHomeLauncherIntent()
-                13 -> executeLLauncherIntent()
-                14 -> executeLucidLauncherIntent()
-                15 -> executeMiniLauncherIntent()
-                16 -> executeNextLauncherIntent()
-                17 -> executeNovaLauncherIntent()
-                19 -> executeSLauncherIntent()
-                20 -> executeSmartLauncherIntent()
-                21 -> executeSmartLauncherProIntent()
-                22 -> executeSoloLauncherIntent()
-                23 -> executeTsfLauncherIntent()
-                24 -> executeUniconIntent()
+                6 -> executeGoLauncherIntent()
+                7, 18 -> executeIconPacksNotSupportedIntent()
+                8 -> executeHoloLauncherIntent()
+                9 -> executeHoloLauncherICSIntent()
+                10 -> executeKkLauncherIntent()
+                11 -> executeLgHomeLauncherIntent()
+                12 -> executeLLauncherIntent()
+                13 -> executeLawnchairIntent()
+                14 -> executeLineageOSThemeEngineIntent()
+                15 -> executeLucidLauncherIntent()
+                16 -> executeMiniLauncherIntent()
+                17 -> executeNextLauncherIntent()
+                19 -> executeNovaLauncherIntent()
+                20 -> executeSLauncherIntent()
+                21 -> executeSmartLauncherIntent()
+                22 -> executeSmartLauncherProIntent()
+                23 -> executeSoloLauncherIntent()
+                24 -> executeTsfLauncherIntent()
+                25 -> executeUniconIntent()
             }
         }
     }
@@ -280,6 +284,12 @@ private fun Context.executeLgHomeLauncherIntent() {
 private fun Context.executeLLauncherIntent() {
     val l = Intent("com.l.launcher.APPLY_ICON_THEME", null)
     l.putExtra("com.l.launcher.theme.EXTRA_PKG", packageName)
+    startActivity(l)
+}
+
+private fun Context.executeLawnchairIntent() {
+    val l = Intent("ch.deletescape.lawnchair.APPLY_ICONS", null)
+    l.putExtra("packageName", packageName)
     startActivity(l)
 }
 
