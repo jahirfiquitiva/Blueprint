2eaab89bc4b2400efec70caed600161d2a6b9c55
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2018-12-24 15:42:47 -0500

    Update

--- app/src/main/kotlin/jahirfiquitiva/apps/blueprint/demo/MuzeiService.kt
+++ app/src/main/kotlin/jahirfiquitiva/apps/blueprint/demo/MuzeiService.kt
@@ -15,7 +15,6 @@
  */
 package jahirfiquitiva.apps.blueprint.demo
 
-import jahirfiquitiva.libs.frames.data.services.FramesArtSource
+import jahirfiquitiva.libs.frames.data.services.FramesArtProvider
 
-// TODO: Use a unique name instead of MyBlueprintMuzeiService
-class MuzeiService : FramesArtSource("MyBlueprintMuzeiService")
+class MuzeiService : FramesArtProvider()
