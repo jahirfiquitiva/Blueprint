6f02a1a6718a86144883a05506e70896570589f5
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-04-21 19:54:06 -0500

    Allow customizing supported launchers

--- library/src/main/kotlin/dev/jahir/blueprint/data/models/Launcher.kt
+++ library/src/main/kotlin/dev/jahir/blueprint/data/models/Launcher.kt
@@ -5,30 +5,44 @@ import androidx.annotation.DrawableRes
 import dev.jahir.blueprint.R
 import dev.jahir.blueprint.extensions.blueprintFormat
 import dev.jahir.blueprint.extensions.clean
+import dev.jahir.frames.extensions.context.stringArray
 import dev.jahir.kuper.extensions.isAppInstalled
 
 @Suppress("ArrayInDataClass", "unused")
 enum class Launcher(
+    val key: String,
     val appName: String,
     val packageNames: Array<String>,
     @DrawableRes val icon: Int,
     val isActuallySupported: Boolean = true
 ) {
-    ACTION("Action Launcher", arrayOf("com.actionlauncher.playstore"), R.drawable.ic_action),
-    ADW("ADW Launcher", arrayOf("org.adw.launcher"), R.drawable.ic_adw),
-    ADW_EX("ADW Ex Launcher", arrayOf("org.adwfreak.launcher"), R.drawable.ic_adw_ex),
-    APEX("Apex Launcher", arrayOf("com.anddoes.launcher"), R.drawable.ic_apex),
-    GO("Go Launcher", arrayOf("com.gau.go.launcherex"), R.drawable.ic_go),
+    ACTION(
+        "action",
+        "Action Launcher",
+        arrayOf("com.actionlauncher.playstore"),
+        R.drawable.ic_action
+    ),
+    ADW("adw", "ADW Launcher", arrayOf("org.adw.launcher"), R.drawable.ic_adw),
+    ADW_EX("adwex", "ADW Ex Launcher", arrayOf("org.adwfreak.launcher"), R.drawable.ic_adw_ex),
+    APEX("apex", "Apex Launcher", arrayOf("com.anddoes.launcher"), R.drawable.ic_apex),
+    GO("go", "Go Launcher", arrayOf("com.gau.go.launcherex"), R.drawable.ic_go),
     GOOGLE_NOW(
+        "googlenow",
         "Google Now Launcher",
         arrayOf("com.google.android.launcher"),
         R.drawable.ic_google_now,
         false
     ),
-    HOLO("Holo Launcher", arrayOf("com.mobint.hololauncher"), R.drawable.ic_holo),
-    HOLO_ICS("Holo Launcher ICS", arrayOf("com.mobint.hololauncher.hd"), R.drawable.ic_holo_ics),
-    LG_HOME("LG Home", arrayOf("com.lge.launcher2"), R.drawable.ic_lg_home),
+    HOLO("holo", "Holo Launcher", arrayOf("com.mobint.hololauncher"), R.drawable.ic_holo),
+    HOLO_ICS(
+        "holoics",
+        "Holo Launcher ICS",
+        arrayOf("com.mobint.hololauncher.hd"),
+        R.drawable.ic_holo_ics
+    ),
+    LG_HOME("lg", "LG Home", arrayOf("com.lge.launcher2"), R.drawable.ic_lg_home),
     LAWNCHAIR(
+        "lawnchair",
         "Lawnchair",
         arrayOf(
             "ch.deletescape.lawnchair.plah",
@@ -39,6 +53,7 @@ enum class Launcher(
         R.drawable.ic_lawnchair
     ),
     LINEAGE_OS(
+        "lineageos",
         "LineageOS Theme Engine",
         arrayOf(
             "org.cyanogenmod.theme.chooser",
@@ -47,20 +62,26 @@ enum class Launcher(
         ),
         R.drawable.ic_lineageos_theme_engine
     ),
-    LUCID("Lucid Launcher", arrayOf("com.powerpoint45.launcher"), R.drawable.ic_lucid),
-    NIAGARA("Niagara Launcher", arrayOf("bitpit.launcher"), R.drawable.ic_niagara),
-    NOVA("Nova Launcher", arrayOf("com.teslacoilsw.launcher"), R.drawable.ic_nova),
+    LUCID("lucid", "Lucid Launcher", arrayOf("com.powerpoint45.launcher"), R.drawable.ic_lucid),
+    NIAGARA("niagara", "Niagara Launcher", arrayOf("bitpit.launcher"), R.drawable.ic_niagara),
+    NOVA("nova", "Nova Launcher", arrayOf("com.teslacoilsw.launcher"), R.drawable.ic_nova),
     PIXEL(
+        "pixel",
         "Pixel Launcher",
         arrayOf("com.google.android.apps.nexuslauncher"),
         R.drawable.ic_pixel,
         false
     ),
-    POSIDON("Posidon Launcher", arrayOf("posidon.launcher"), R.drawable.ic_posidon),
-    SMART("Smart Launcher", arrayOf("ginlemon.flowerfree"), R.drawable.ic_smart),
-    SMART_PRO("Smart Launcher Pro", arrayOf("ginlemon.flowerpro"), R.drawable.ic_smart_pro),
-    SOLO("Solo Launcher", arrayOf("home.solo.launcher.free"), R.drawable.ic_solo),
-    TSF("TSF Launcher", arrayOf("com.tsf.shell"), R.drawable.ic_tsf);
+    POSIDON("posidon", "Posidon Launcher", arrayOf("posidon.launcher"), R.drawable.ic_posidon),
+    SMART("smart", "Smart Launcher", arrayOf("ginlemon.flowerfree"), R.drawable.ic_smart),
+    SMART_PRO(
+        "smartpro",
+        "Smart Launcher Pro",
+        arrayOf("ginlemon.flowerpro"),
+        R.drawable.ic_smart_pro
+    ),
+    SOLO("solo", "Solo Launcher", arrayOf("home.solo.launcher.free"), R.drawable.ic_solo),
+    TSF("tsf", "TSF Launcher", arrayOf("com.tsf.shell"), R.drawable.ic_tsf);
 
     val cleanAppName: String
         get() = appName.replace("launcher", "", true).clean().blueprintFormat()
@@ -77,7 +98,10 @@ enum class Launcher(
 
     companion object {
         fun getSupportedLaunchers(context: Context? = null): ArrayList<Pair<Launcher, Boolean>> {
+            val actuallySupportedLaunchers =
+                context?.stringArray(R.array.supported_launchers).orEmpty()
             val default = values().sortedBy { it.appName }
+                .filter { def -> actuallySupportedLaunchers.any { sup -> sup == def.key } }
             val pairs =
                 default.map { Pair(it, it.isInstalled(context)) }.sortedByDescending { it.second }
             return ArrayList(pairs)
