2db342f49ac5c41fef113169b6af6fb534283caa
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-05-25 13:30:29 -0500

    Update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,13 +2,13 @@
 
 object Versions {
     // Plugins
-    const val gradle = "3.6.2"
+    const val gradle = "3.6.3"
     const val kotlin = "1.3.72"
     const val jitpack = "2.1"
 
     // OneSignal
     const val oneSignalPlugin = "0.12.7"
-    const val oneSignal = "3.13.2"
+    const val oneSignal = "3.14.0"
 
     // App
     const val minSdk = 21
@@ -16,7 +16,7 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Kuper
-    const val kuper = "2.1.9"
+    const val kuper = "2.2.0"
 
     // Adaptive Icons
     const val adaptiveIcons = "a0884c7"
