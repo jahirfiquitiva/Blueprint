49382654b230130bb872d29829d82448c3d00ff8
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-05-26 13:10:51 -0500

    Update Versions.kt

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,13 +2,13 @@
 
 object Versions {
     // Plugins
-    const val gradle = "3.6.2"
+    const val gradle = "3.6.3"
     const val kotlin = "1.3.72"
     const val jitpack = "2.1"
 
     // OneSignal
     const val oneSignalPlugin = "0.12.7"
-    const val oneSignal = "3.13.2"
+    const val oneSignal = "3.14.0"
 
     // App
     const val minSdk = 21
@@ -16,5 +16,5 @@ object Versions {
     const val buildTools = "29.0.3"
 
     // Blueprint
-    const val blueprint = "2.0.9"
+    const val blueprint = "2.1.0"
 }
