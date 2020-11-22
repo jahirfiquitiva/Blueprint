a14b96aa1a5ea696110e6037578e14825bc1bbc1
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2020-11-22 12:44:13 -0500

    Update MyApp.kt

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.jahir.blueprint.app"
-    const val version = 215
-    const val versionName = "2.1.5"
+    const val version = 216
+    const val versionName = "2.1.6"
 }
