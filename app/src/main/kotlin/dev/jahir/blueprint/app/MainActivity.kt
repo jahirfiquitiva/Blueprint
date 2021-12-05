bf0f7154a5543568fb1cb5ff84eb9ac6b15d813a
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2021-12-04 21:48:36 -0500

    Update Blueprint to v 2.2.5

--- app/src/main/kotlin/dev/jahir/blueprint/app/MainActivity.kt
+++ app/src/main/kotlin/dev/jahir/blueprint/app/MainActivity.kt
@@ -40,4 +40,7 @@ class MainActivity : BottomNavigationBlueprintActivity() {
 
     override fun defaultTheme(): Int = R.style.MyApp_Default
     override fun amoledTheme(): Int = R.style.MyApp_Default_Amoled
+
+    override fun defaultMaterialYouTheme(): Int = R.style.MyApp_Default_MaterialYou
+    override fun amoledMaterialYouTheme(): Int = R.style.MyApp_Default_Amoled_MaterialYou
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,18 +2,18 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.0.0"
-    const val kotlin = "1.5.21"
+    const val gradle = "7.0.3"
+    const val kotlin = "1.5.31"
 
     // OneSignal
-    const val oneSignalPlugin = "0.13.4"
-    const val oneSignal = "4.4.2"
+    const val oneSignalPlugin = "0.14.0"
+    const val oneSignal = "4.6.3"
 
     // App
     const val minSdk = 21
-    const val targetSdk = 30
+    const val targetSdk = 31
     const val buildTools = "30.0.2"
 
     // Blueprint
-    const val blueprint = "2.2.4"
+    const val blueprint = "2.2.5"
 }
