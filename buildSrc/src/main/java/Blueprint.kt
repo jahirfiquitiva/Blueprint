42497682f41a97570ef1f6bef20e3fbe28ee32d7
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-02-13 19:09:35 -0500

    Update

--- buildSrc/src/main/java/Blueprint.kt
+++ buildSrc/src/main/java/Blueprint.kt
@@ -2,6 +2,6 @@
 
 object Blueprint {
     const val appId = "dev.jahir.blueprint.app"
-    const val version = 226
-    const val versionName = "2.2.6"
+    const val version = 228
+    const val versionName = "2.2.8"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,21 +2,21 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.0.3"
+    const val gradle = "7.1.1"
     const val kotlin = "1.5.31"
     const val sonatype = "1.1.0"
 
     // OneSignal
     const val oneSignalPlugin = "0.14.0"
-    const val oneSignal = "4.6.3"
+    const val oneSignal = "4.6.7"
 
     // App
     const val minSdk = 21
     const val targetSdk = 31
-    const val buildTools = "30.0.2"
+    const val buildTools = "30.0.3"
 
     // Kuper
-    const val kuper = "2.3.6"
+    const val kuper = "2.3.8"
 
     // Adaptive Icons
     const val adaptiveIcons = "1.0.0"
