acb4e78af6c540185a7b9e3ff4ace1e83c41c60e
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2022-02-13 19:27:41 -0500

    Update to Blueprint 2.2.8

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,18 +2,18 @@
 
 object Versions {
     // Plugins
-    const val gradle = "7.0.3"
+    const val gradle = "7.1.1"
     const val kotlin = "1.5.31"
 
     // OneSignal
     const val oneSignalPlugin = "0.14.0"
-    const val oneSignal = "4.6.3"
+    const val oneSignal = "4.6.7"
 
     // App
     const val minSdk = 21
     const val targetSdk = 31
-    const val buildTools = "30.0.2"
+    const val buildTools = "30.0.3"
 
     // Blueprint
-    const val blueprint = "2.2.6"
+    const val blueprint = "2.2.8"
 }
