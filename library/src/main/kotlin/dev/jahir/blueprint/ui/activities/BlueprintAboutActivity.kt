59571882c46cbb08aa2f29c24a66dc49d6287647
Patryk Michalik <contact@patrykmichalik.com>
2022-07-26 17:23:42 +0200

    `BlueprintAboutActivity.kt`: Import `dev.jahir.blueprint.BuildConfig` instead of `dev.jahir.kuper.BuildConfig`

--- library/src/main/kotlin/dev/jahir/blueprint/ui/activities/BlueprintAboutActivity.kt
+++ library/src/main/kotlin/dev/jahir/blueprint/ui/activities/BlueprintAboutActivity.kt
@@ -1,7 +1,7 @@
 package dev.jahir.blueprint.ui.activities
 
 import dev.jahir.frames.ui.activities.AboutActivity
-import dev.jahir.kuper.BuildConfig
+import dev.jahir.blueprint.BuildConfig
 
 class BlueprintAboutActivity : AboutActivity() {
     override val dashboardName = BuildConfig.DASHBOARD_NAME
