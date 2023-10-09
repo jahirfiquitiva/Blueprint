9e7b4b7991ec0e546defba066eaf02effb2becd2
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2023-10-08 19:57:54 -0500

    Update to Blueprint 2.3.9

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,18 +2,18 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.1.0"
-    const val kotlin = "1.9.0"
-    const val ksp = "$kotlin-1.0.12"
+    const val gradle = "8.1.1"
+    const val kotlin = "1.9.10"
+    const val ksp = "$kotlin-1.0.13"
 
     // OneSignal
     const val oneSignal = "4.8.6"
 
     // App
     const val minSdk = 21
-    const val targetSdk = 33
-    const val buildTools = "33.0.2"
+    const val targetSdk = 34
+    const val buildTools = "34.0.0"
 
     // Blueprint
-    const val blueprint = "2.3.7"
+    const val blueprint = "2.3.9"
 }
