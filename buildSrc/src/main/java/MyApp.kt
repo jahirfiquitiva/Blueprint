72e1edd05b0b5c63ca15a219842b75723a1cb1f5
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2025-01-05 18:00:52 -0500

    Update to Blueprint 2.4.3

--- buildSrc/src/main/java/MyApp.kt
+++ buildSrc/src/main/java/MyApp.kt
@@ -2,6 +2,6 @@
 
 object MyApp {
     const val appId = "dev.jahir.blueprint.app"
-    const val version = 242
-    const val versionName = "2.4.2"
+    const val version = 243
+    const val versionName = "2.4.3"
 }
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,18 +2,18 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.5.0"
-    const val kotlin = "2.0.0"
-    const val ksp = "$kotlin-1.0.22"
+    const val gradle = "8.6.1"
+    const val kotlin = "2.1.0"
+    const val ksp = "$kotlin-1.0.29"
 
     // OneSignal
     const val oneSignal = "4.8.10"
 
     // App
     const val minSdk = 21
-    const val targetSdk = 34
-    const val buildTools = "34.0.0"
+    const val targetSdk = 35
+    const val buildTools = "35.0.0"
 
     // Blueprint
-    const val blueprint = "2.4.2"
+    const val blueprint = "2.4.3"
 }
