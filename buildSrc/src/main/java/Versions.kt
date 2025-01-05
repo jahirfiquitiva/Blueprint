b78b2cfc829868d08e1128727ff1f70ed08d0b32
Jahir Fiquitiva <jahir.fiquitiva@gmail.com>
2025-01-05 17:25:33 -0500

    chore: update dependencies

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -2,22 +2,22 @@
 
 object Versions {
     // Plugins
-    const val gradle = "8.5.0"
-    const val kotlin = "2.0.0"
+    const val gradle = "8.6.1"
+    const val kotlin = "2.1.0"
     const val sonatype = "2.0.0"
-    const val ksp = "$kotlin-1.0.22"
+    const val ksp = "$kotlin-1.0.29"
 
     // OneSignal
     const val oneSignal = "4.8.10"
 
     // App
     const val minSdk = 21
-    const val targetSdk = 34
-    const val buildTools = "34.0.0"
+    const val targetSdk = 35
+    const val buildTools = "35.0.0"
 
     // Kuper
-    const val kuper = "2.5.2"
+    const val kuper = "2.5.3"
 
     // Adaptive Icons
-    const val adaptiveIcons = "1.0.1"
+    const val adaptiveIcons = "1.0.2"
 }
